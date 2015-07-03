fc2c67a6555ae355525a3b7d5bbf999670dd5715
chengyuhui <chengyuhui1@gmail.com>
2015-07-03 18:56:15 +0800

    Use cheerio to make things clearer.

--- src/sites/sites/pixiv.js
+++ src/sites/sites/pixiv.js
@@ -53,50 +53,21 @@ module.exports = {
 			// 漫画等
 			// 原本可以直接使用$(html)的方式构建DOM树进行解析，但是这样做会导致不必要的资源浪费
 			// （浏览器会把所有页面的引用资源下载下来，即使没有插入进document）
-			const {Parser} = require('htmlparser2');
+			const {load} = require('cheerio');
 			const mangaUrl = document.location.toString().split('medium').join('manga');
 
-			let mangaHtml = await $.get(mangaUrl);
+			const mangaHtml = await $.get(mangaUrl);
 			// 获取所有到大图查看页的地址
 			// 另外一种实现方式是直接通过缩略图地址和正则表达式替换获取大图地址，但稳定性不好
-			let mangaBigLinks = await new Promise(function (resolve, reject) {
-				var links = [];
-				var p = new Parser({
-					onopentag: function (name, attrib) {
-						if (name === 'a' && attrib.class && attrib.class.includes('full-size-container')) {
-							links.push(attrib.href);
-						}
-					},
-					onend: function () {
-						resolve(links);
-					},
-					onerror: reject
-				}, {decodeEntities: true});
-				// 始终设置decodeEntities: true，
-				// 原因见https://github.com/fb55/htmlparser2/issues/105
+			const mangaBigLinks = load(mangaHtml)('a.full-size-container')
+			.map((i, el) => el.attribs.href ).get();
 
-				p.write(mangaHtml);
-				p.end();
-			});
+			// 获取大图地址
+			// 用R.nAry将$.get接受的参数数量改为1
+			// （原始mapper签名为item, index, arrayLength）
+			pages = await Promise.map(mangaBigLinks, R.nAry(1, $.get))
+			.map(html => load(html)('img').attr('src'));
 
-			pages = await Promise.all(mangaBigLinks.map(async function (link) {
-				var html = await $.get(link);
-
-				return await new Promise(function (resolve, reject) {
-					var p = new Parser({
-						onopentag: function (name, attrib) {
-							// 暂时认为每个页面第一个img就是大图
-							if (name === 'img') {
-								resolve(attrib.src);
-							}
-						},
-						onerror: reject
-					}, {decodeEntities: true});
-
-					p.write(html);
-					p.end();
-				});
-			}));
 		}
 		return pages;
 	},
